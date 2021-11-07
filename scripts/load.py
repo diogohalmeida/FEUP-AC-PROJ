6b576bfcb7a9446f3b7635ed576a6bdf91cac375
MechJM <matosjoaops@protonmail.com>
2021-11-07 22:28:39 +0000

    Renaming columns when loading data

--- scripts/load.py
+++ scripts/load.py
@@ -3,11 +3,13 @@ import pandas as pd
 print("::::LOADING::::")
 
 account = pd.read_csv("dataset/account.csv", sep=";")
-print("Loading account.csv")
+account.rename(columns={"date": "date_account"}, inplace=True)
+print("Loading account.csv renaming ['date'] to ['date_account']")
 account.to_csv("dev/account_loaded.csv", index=False)
 
 card = pd.read_csv("dataset/card_train.csv", sep=";")
-print("Loading card_train.csv")
+card.rename(columns={"type": "type_card", "issued": "date_card"}, inplace=True)
+print("Loading card_train.csv renaming ['type', 'issued'] to ['type_card', 'date_card']")
 card.to_csv("dev/card_loaded.csv", index=False)
 
 client = pd.read_csv("dataset/client.csv", sep=";")
@@ -15,7 +17,8 @@ print("Loading client.csv")
 client.to_csv("dev/client_loaded.csv", index=False)
 
 disp = pd.read_csv("dataset/disp.csv", sep=";")
-print("Loading disp.csv")
+disp.rename(columns={"type": "type_disp"})
+print("Loading disp.csv renaming ['type'] to ['type_disp']")
 disp.to_csv("dev/disp_loaded.csv", index=False)
 
 district = pd.read_csv("dataset/district.csv", sep=";")
@@ -24,9 +27,11 @@ print("Loading district.csv renaming ['code ', 'name '] to ['district_id', 'name
 district.to_csv("dev/district_loaded.csv", index=False)
 
 loan = pd.read_csv("dataset/loan_train.csv", sep=";")
-print("Loading loan_train.csv")
+loan.rename(columns={"date": "date_loan", "amount": "amount_loan"})
+print("Loading loan_train.csv renaming ['date', 'amount'] to ['date_loan', 'amount_loan']")
 loan.to_csv("dev/loan_loaded.csv", index=False)
 
 trans = pd.read_csv("dataset/trans_train.csv", sep=";")
-print("Loading trans_train.csv")
+trans.rename(columns={"date": "date_trans", "type": "type_trans", "amount": "amount_trans"})
+print("Loading trans_train.csv renaming ['date', 'type', 'amount'] to ['date_trans', 'type_trans', 'amount_trans']")
 trans.to_csv("dev/trans_loaded.csv", index=False)
