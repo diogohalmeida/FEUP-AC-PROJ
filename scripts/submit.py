f4b9d7ea93b02a85b94d7a203d8fff567eb3be35
MechJM <matosjoaops@protonmail.com>
2021-11-13 16:18:14 +0000

    Removed scripts

--- scripts/submit.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from datetime import date
-
-submission_df = pd.read_csv("dev/modeled.csv")
-today = date.today()
-
-submission_df.to_csv(f"submissions/{str(today)}.csv", index=False)
